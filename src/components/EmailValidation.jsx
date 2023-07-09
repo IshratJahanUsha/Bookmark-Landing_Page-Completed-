4790d2fdfca52145607f9a0fcbf6cf8e1a706d23
IshratJahanUsha <ishratjahanusha2021@gmail.com>
2023-07-09 21:05:35 +0600

    com12

--- src/components/EmailValidation.jsx
+++ src/components/EmailValidation.jsx
@@ -4,7 +4,8 @@ const EmailValidation = () => {
     const [email, setEmail] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-    const validateEmail = () => {
+    const validateEmail = (e) => {
+        e.preventDefault();
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!email.match(emailRegex)) {
@@ -14,27 +15,24 @@ const EmailValidation = () => {
         }
     };
 
-    // const inputClasses = errorMessage ? 'input-class input-error' : 'input-class';
-
     return (
         <div>
-            <form action="" className="flex flex-col gap-4 md:flex-row md:items-center md:justify-center md:max-w-xl md:mx-auto">
+            <form onSubmit={validateEmail} className="flex flex-col gap-4 md:flex-row md:items-center md:justify-center md:max-w-xl md:mx-auto">
                 <div className='flex flex-col mt-2'>
                     <input
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        onBlur={validateEmail}
                         placeholder="Enter your email"
-                        className={errorMessage ? "input-class input-error" : "input-class"}
-                        // className="w-full py-2 px-4 rounded shadow-lg text-sm border border-red-500 invalid"
+                        className={`text-sm w-full py-2 px-4 outline-none border-2
+                         ${errorMessage ? "border-red-500" : "border-transparent" }`}
                     />
                     <p className='h-2'>
                         <span className='text-red-400 text-sm'>{errorMessage}</span>
                     </p>
                 </div>
 
-                <button className="btn-red px-4 rounded py-2 shadow-lg md:w-40 text-sm">
+                <button type= "submit" className="btn-red px-4 py-2 rounded mt-2 md:mt-4 shadow-lg md:w-40 text-sm">
                     Contact Us
                 </button>
 
